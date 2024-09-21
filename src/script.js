83d3707b4b13b33b8612019ed90adedf1759822e
okumualex <alecky358425@gmail.com>
2024-09-21 17:34:49 +0300

    created div to an append two elements P and span tag

--- src/script.js
+++ src/script.js
@@ -10,25 +10,29 @@ function updateStorage() {
   localStorage.setItem("notes", contentContainer.innerHTML);
 }
 
-btn.addEventListener("click", () => {
-  const inputBox = document.createElement("p");
-  const span = document.createElement("span");
-  inputBox.className = "input-box";
-  inputBox.setAttribute("contenteditable", "true");
-  span.className = "fa fa-trash";
-  span.setAttribute("contenteditable", "false");
-  contentContainer.appendChild(inputBox).appendChild(span);
+btn.addEventListener('click', ()=>{
+  const div = document.createElement('div');
+  const inputBox = document.createElement('p');
+  const span = document.createElement('span');
+  div.className = 'inner-container';
+  inputBox.className = 'input-box';
+  inputBox.setAttribute('contenteditable', 'true');
+  span.className = 'fa fa-trash';
+  contentContainer.appendChild(div);
+  div.appendChild(inputBox);
+  div.appendChild(span);
 });
-contentContainer.addEventListener("click", (e) => {
-  if (e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
+contentContainer.addEventListener('click', (evt)=>{
+  if(evt.target.tagName === 'SPAN'){
+    evt.target.parentElement.remove();
     updateStorage();
-  } else if (e.target.tagName === "P") {
-    const notes = document.querySelectorAll(".input-box");
-    notes.forEach((note) => {
-      note.onkeyup = function () {
+  }
+  else if(evt.target.tagName === 'P'){
+    const notes = document.querySelectorAll('.input-box');
+    notes.forEach(note =>{
+      note.onkeyup = () =>{
         updateStorage();
-      };
-    });
+      }
+    })
   }
 });
